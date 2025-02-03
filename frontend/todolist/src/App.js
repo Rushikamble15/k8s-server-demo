cc29e66f1b5d8831c98dfdf5ac8d5cfdbbc7ec49
Rushikamble15 <119659246+Rushikamble15@users.noreply.github.com>
2025-02-03 18:25:06 +0530

    Update App.js

--- frontend/todolist/src/App.js
+++ frontend/todolist/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-const BACKEND_URL = `http://${window.location.hostname}:30002` // Fixed backend URL
+const BACKEND_URL = `http://${window.location.hostname}:30001` // Fixed backend URL
 
 function App() {
   const [todos, setTodos] = useState([]);
