88a8339a9e0c29010fa71aced3a6a9f76cdbd3f4
Rushikamble15 <119659246+Rushikamble15@users.noreply.github.com>
2025-02-03 12:04:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -119,6 +119,6 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 3001; // Use the environment variable PORT or default to 3001
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Server is running on port ${PORT}`);
 });
